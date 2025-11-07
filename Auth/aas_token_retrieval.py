9f4f9a32531b9093e11896446955a4273716dcb7
BSkando <124605801+BSkando@users.noreply.github.com>
2025-11-07 15:55:22 -0500

    Fix for SSL issues with Python 3.14

--- Auth/aas_token_retrieval.py
+++ Auth/aas_token_retrieval.py
@@ -3,12 +3,35 @@
 #  Copyright © 2024 Leon Böttger. All rights reserved.
 #
 
+import ssl
+import sys
 import gpsoauth
 
 from Auth.auth_flow import request_oauth_account_token_flow
 from Auth.fcm_receiver import FcmReceiver
 from Auth.token_cache import get_cached_value_or_set, set_cached_value
 from Auth.username_provider import get_username, username_string
+
+# Python 3.14 SSL compatibility fix
+if sys.version_info >= (3, 10):
+    import urllib3
+    from urllib3.util.ssl_ import create_urllib3_context
+
+    # Monkey-patch urllib3 to use a more lenient SSL context
+    class CustomSSLContextHTTPAdapter(urllib3.util.ssl_.SSLContext):
+        pass
+
+    original_create_urllib3_context = create_urllib3_context
+
+    def patched_create_urllib3_context(*args, **kwargs):
+        context = original_create_urllib3_context(*args, **kwargs)
+        # Allow TLS 1.2+ to work with older servers
+        context.minimum_version = ssl.TLSVersion.TLSv1_2
+        context.options |= ssl.OP_LEGACY_SERVER_CONNECT
+        return context
+
+    urllib3.util.ssl_.create_urllib3_context = patched_create_urllib3_context
+    urllib3.util.create_urllib3_context = patched_create_urllib3_context
 
 
 def _generate_aas_token():
